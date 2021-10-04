9018f0cca01934896f2e5d4b162d71413626de32
Fuller Lajos Daniel <laloka011996@gmail.com>
2021-10-04 17:31:24 +0200

    Add project files.

---
+++ DDB2DA_HFT_2021221.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DDB2DA_HFT_2021221.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
