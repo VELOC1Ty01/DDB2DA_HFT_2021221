b1445ceed29a42b9100010ecff77b16caed6a16d
Füller Lajos Dániel <lalo.fuller@gmail.com>
2021-10-15 16:38:13 +0200

    DbContext created, Model classes created

---
+++ DDB2DA_HFT_2021221.Models/Driver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDB2DA_HFT_2021221.Models
+{
+    [Table("driver")]
+    class Driver
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string SurName { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [ForeignKey("Team")]
+        public int TeamId { get; set; }
+       
+
+    }
+}
---
+++ DDB2DA_HFT_2021221.Models/Team.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDB2DA_HFT_2021221.Models
+{
+    [Table("team")]
+    class Team
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [MaxLength(60)]
+        [Required]
+        public string Name { get; set; }
+
+        [NotMapped]
+        public virtual ICollection<Driver> Drivers { get; set; }
+
+        public Team()
+        {
+            Drivers = new HashSet<Driver>();
+        }
+
+    }
+}
