1ec7d3045dead0c25024a24251cb5d5e9fc4d618
Füller Lajos Dániel <lalo.fuller@gmail.com>
2021-10-15 16:13:04 +0200

    DbContext created

---
+++ DDB2DA_HFT_2021221.Data/F1DbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDB2DA_HFT_2021221.Data
+{
+    class F1DbContext : DbContext
+    {
+
+        public F1DbContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured) {
+                optionsBuilder
+                    .UseLazyLoadingProxies()
+                    .UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database.mdf;Integrated Security=True");
+            }
+        }
+
+
+
+    }
+}
