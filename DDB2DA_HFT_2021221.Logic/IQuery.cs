4294eff050598149e26e36489f0bdf3fedfc4de6
Füller Lajos Dániel <lalo.fuller@gmail.com>
2021-12-08 23:24:21 +0100

    Added methods to IQuery interface

--- DDB2DA_HFT_2021221.Logic/IQuery.cs
+++ DDB2DA_HFT_2021221.Logic/IQuery.cs
@@ -13,14 +13,30 @@ namespace DDB2DA_HFT_2021221.Logic
         /// Calculates a team's points by the sum of their drivers points.
         /// </summary>
         /// <returns></returns>
-        public IEnumerable<Team> GetTeamPointsFromDrivers();
+        public IEnumerable<Team> GetTeamPointsFromDrivers(int teamId);
 
         /// <summary>
         /// Lists drivers of a team.
         /// </summary>
         /// <returns></returns>
-        public IEnumerable<Driver> GetDriversFromTeam();
+        public IEnumerable<Driver> GetDriversFromTeam(int teamId);
 
+        /// <summary>
+        /// Gets teams who participated in all GPs.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Team> GetAllOutTeams();
 
+        /// <summary>
+        /// Get teams who did not participate in all GPs.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Team> GetTeamsWhoSkippedGP();
+
+        /// <summary>
+        /// Gets all the races a driver participated in.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<GrandPrix> GetDriverRaces(int driverId);
     }
 }
