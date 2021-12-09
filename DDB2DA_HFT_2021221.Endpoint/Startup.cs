74c7f347dc23f3f326c605d8c7e38886321e15d6
Füller Lajos Dániel <lalo.fuller@gmail.com>
2021-12-09 15:55:10 +0100

    RestAPI startup config set.

--- DDB2DA_HFT_2021221.Endpoint/Startup.cs
+++ DDB2DA_HFT_2021221.Endpoint/Startup.cs
@@ -1,6 +1,10 @@
+using DDB2DA_HFT_2021221.Data;
+using DDB2DA_HFT_2021221.Logic;
+using DDB2DA_HFT_2021221.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,10 +16,32 @@ namespace DDB2DA_HFT_2021221.Endpoint
 {
     public class Startup
     {
+         public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+
+            services.AddSingleton<F1DbContext, F1DbContext>();
+
+            services.AddSingleton<IDriverLogic, DriverLogic>();
+            services.AddSingleton<IDriverRepository, DriverRepository>();
+
+            services.AddSingleton<ITeamLogic, TeamLogic>();
+            services.AddSingleton<ITeamRepository, TeamRepository>();
+
+            services.AddSingleton<IGrandPrixLogic, GrandPrixLogic>();
+            services.AddSingleton<IGrandPrixRepository, GrandPrixRepository>();
+
+            services.AddSingleton<IQueryLogic, QueryLogic>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,15 +51,14 @@ namespace DDB2DA_HFT_2021221.Endpoint
             {
                 app.UseDeveloperExceptionPage();
             }
+            else 
+                app.UseExceptionHandler("/Error");
 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
